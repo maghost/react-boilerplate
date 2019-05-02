3b3aef906fe40af4c8331d7841ef9b01f972c03f
cl4pper <cl4pper@gmail.com>
2019-05-02 15:12:01 -0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	// original file to be transpiled
+	entry: path.resolve(__dirname, 'src', 'index.js'),
+	// destiny of transpiled file
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	// directory watched by webpack-dev-server when any update comes
+	devServer: {
+		contentBase: path.resolve(__dirname, "public"),
+	},
+	// config. rules to transpiling project
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
+};
