0d0af68605bd0687874f2d79d8f63dfa568abb11
matheus.pereira@gamersclub.com.br <matheus.pereira@gamersclub.com.br>
2019-05-06 17:58:33 -0300

    [master] Update project to fit redux inside it

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component, Fragment } from 'react'
 import { Provider } from 'react-redux'
 
+// STYLE
+import './app.scss'
+
 // STORE
 import store from './store'
 
 // COMPONENTS
 import PageTitle from './components/PageTitle/PageTitle'
 import RoadmapList from './components/RoadmapList/RoadmapList';
+import Counter from './components/Counter/Counter';
 
 export default class App extends Component {
 	// no CONSTRUCTOR - if you do not initialize state or do not bind methods
 	render() {
 		return (
 			<Provider store={store}>
-				<Fragment>
+				<div className="app">
 					<PageTitle title="React Component" />
+					<Counter></Counter>
 					<RoadmapList />
-				</Fragment>
+				</div>
 			</Provider>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -5,23 +5,7 @@ INTO HTML INDEX FILE.
 import React from 'react'
 import { render } from 'react-dom'
 
-// STYLE
-import './app.scss'
-
 // COMPONENTS
-import PageTitle from './components/PageTitle/PageTitle'
-import Counter from './components/Counter/Counter';
-
-class App extends Component {
-	// no CONSTRUCTOR - if you do not initialize state or do not bind methods
-	render() {
-		return (
-			<div className="app">
-				<PageTitle title="React Component" />
-				<Counter></Counter>
-			</div>
-		)
-	}
-}
+import App from './App'
 
 render(<App/>, document.getElementById('app'))
